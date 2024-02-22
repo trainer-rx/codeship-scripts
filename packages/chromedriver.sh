53591c865af086fba2ef5f7e005561d712a54ec2
Bryan Yee <yee.bryan.c@gmail.com>
2024-02-22 14:13:50 -0600

    Update chromedriver source location for the stable public domain

--- packages/chromedriver.sh
+++ packages/chromedriver.sh
@@ -13,7 +13,7 @@ set -e
 CACHED_DOWNLOAD="${HOME}/cache/chromedriver_linux64_${CHROMEDRIVER_VERSION}.zip"
 
 rm -rf "${HOME}/bin/chromedriver"
-wget --continue --output-document "${CACHED_DOWNLOAD}" "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://storage.googleapis.com/chrome-for-testing-public/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip"
 unzip -o "${CACHED_DOWNLOAD}" -d "${HOME}/bin"
 
 export PATH="${HOME}/bin/chromedriver-linux64:$PATH"
